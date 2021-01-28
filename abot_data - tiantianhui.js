64a1830e05fb0cf2f06655cf91ce983b3738b1c2
longmix <xiaoa@abot.cn>
2021-01-28 10:07:59 +0000

    版本升级

--- abot_data - tiantianhui.js
+++ abot_data - tiantianhui.js
@@ -4,14 +4,14 @@ const globalData = {
 		o2owaimai_server_url: 'https://app.tseo.cn/o2owaimai/index.php/',
 		socket_server: 'https://socketio.tseo.cn',
 		
-		version_number:'Ver 1.8.1 Build 362',
-		version_code:362,
+		version_number:'Ver 1.8.2 Build 365',
+		version_code:365,
 		
 		force_sellerid:1,
 	
 		xiaochengxu_appid: 'wx47cf36b7d0a0f792',			//微信小程序的appid
 		xiaochengxu_account: 'gh_3deff1aa52cb',				//微信小程序的账号
-		weixin_open_platform_appid: 'wx6a3b6ecb6fcff9a9',	//微信开放平台中登记的APP对应的appid（务必与manifest中的配置一致）
+		weixin_open_platform_appid: 'wx6939a00416c5d3aa--',	//微信开放平台中登记的APP对应的appid（务必与manifest中的配置一致）
 		
 		//通版商城
 		default_shopname: '天天汇',
@@ -22,8 +22,8 @@ const globalData = {
 		//=======以下配置项在发布版本的时候确定，服务器端配置无效===
 		
 		//平台相关的配置
-		current_platform : 'app-android',
-		//current_platform : 'app-ios',
+		//current_platform : 'app-android',
+		current_platform : 'app-ios',
 		//current_platform : 'mp-weixin',
 		//current_platform : 'mp-baidu',
 		//current_platform : 'mp-alipay',
