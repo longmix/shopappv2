d46ae5e798b82703497476d153f28d584bae2471
longmix <xiaoa@abot.cn>
2021-05-06 04:13:57 +0000

    更新安全熊APP的底部导航菜单，增加购物车和商品分类。

--- abot_data - anquanxiong.js
+++ abot_data - anquanxiong.js
@@ -4,8 +4,8 @@ const globalData = {
 		o2owaimai_server_url: 'https://app.tseo.cn/o2owaimai/index.php/',
 		socket_server: 'https://socketio.tseo.cn',
 		
-		version_number:'Ver 1.7.7 Build 255',
-		version_code:255,
+		version_number:'Ver 1.7.8 Build 256',
+		version_code:256,
 		
 		force_sellerid:1,
 	
