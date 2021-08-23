7a18fe3e5114f48cfab470d10972de68a1567658
longmix <xiaoa@abot.cn>
2021-08-23 10:08:08 +0000

    增加了安卓版本隐私的一些设置项

--- abot_data - yanyubaoappv2.js
+++ abot_data - yanyubaoappv2.js
@@ -4,8 +4,8 @@ const globalData = {
 		o2owaimai_server_url: 'https://app.tseo.cn/o2owaimai/index.php/',
 		socket_server: 'https://socketio.tseo.cn',
 		
-		version_number:'Ver 3.1.0 Build 893',
-		version_code:893,
+		version_number:'Ver 3.1.0 Build 897',
+		version_code:897,
 		
 		force_sellerid:1,
 	
