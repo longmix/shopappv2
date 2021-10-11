e145ef25dcb1f0b91d1d39a80ebd271bf841d50f
longmix <xiaoa@abot.cn>
2021-10-11 06:48:42 +0000

    更新小程序模板至2.1.2.316

--- abot_data - shopapp.js
+++ abot_data - shopapp.js
@@ -4,8 +4,8 @@ const globalData = {
 		o2owaimai_server_url: 'https://app.tseo.cn/o2owaimai/index.php/',
 		socket_server: 'https://socketio.tseo.cn',
 		
-		version_number:'Ver 2.1.1 Build 312',
-		version_code:312,
+		version_number:'Ver 2.1.2 Build 316',
+		version_code:316,
 		
 		force_sellerid:0,
 		
