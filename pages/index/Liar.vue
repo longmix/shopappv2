96a621f281ee4f31bfe5830def787b4251f16785
longmix <xiaoa@abot.cn>
2022-01-23 02:02:28 +0000

    增加页面index/Liar，用于兼容 xiaochengxu/shopapp 项目的默认页面，没有具体的功能

---
+++ pages/index/Liar.vue
@@ -0,0 +1,41 @@
+<template>
+</template>
+
+<script>
+	/**
+	 * //用于兼容 xiaochengxu/shopapp 项目的默认页面，没有具体的功能
+	 */
+	export default {
+		data() {
+			return {
+			    
+			};
+		},
+		onLoad(options) {
+			
+			console.log('aaaaaaaaaaaaaa');
+			
+			
+			
+			//通过  redirectTo 跳转，模板页面应该添加一个返回首页的链接，不然在APP中会出现跳不回去的情况
+			uni.reLaunch({
+				url: '/pages/index/index',
+				fail: (res) => {
+					console.log(res);
+				},
+				success: (res) => {
+					console.log(res);
+				}
+			})
+			
+		},
+		onShow: () => {
+			console.log('index/Liar onShow !');
+			
+			
+		}
+	}
+</script>
+
+<style>
+</style>
